8dd8e26543edbfd3915220c69f4e98de552dccc3
jojohve <joseph.laluna@outlook.it>
2024-06-24 14:50:20 +0200

    header

--- wp-content/themes/Depyl-theme/header.php
+++ wp-content/themes/Depyl-theme/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
     <?php wp_head(); ?>
 </head>
 
