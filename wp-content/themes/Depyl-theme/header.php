9d79bfe47abae0675742cd6bc062acb26213fa4a
jojohve <joseph.laluna@outlook.it>
2024-06-26 12:37:45 +0200

    style

--- wp-content/themes/Depyl-theme/header.php
+++ wp-content/themes/Depyl-theme/header.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <link rel="shortcut icon" href="/assets/images/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/wp-content/themes/depyl-theme/assets/images/logo.png" type="image/x-icon">
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
     <header>
-        <img src="assets/images/logo.png" alt="logo">
+        <img src="/wp-content/themes/depyl-theme/assets/images/logo.png" alt="logo" class="logo">
         <h1><?php bloginfo('name'); ?></h1>
         <p><?php bloginfo('description'); ?></p>
         <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
